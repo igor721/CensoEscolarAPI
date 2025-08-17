77db519f1eeb0cf4af8d066100ec74fa68822365
Igor_Gabriel <86084607+igor721@users.noreply.github.com>
2025-08-16 21:20:27 -0300

    Delete schemas.sql

--- schemas.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS tb_instituicao;
-
-CREATE TABLE tb_instituicao (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    no_entidade TEXT NOT NULL,
-    co_entidade INTEGER NOT NULL,
-    qt_mat_bas INTEGER NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
